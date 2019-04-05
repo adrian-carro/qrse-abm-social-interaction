cda65b5da7b09325142a00e31b5292447098739a
Adrián Carro <adrian.carro@gmail.com>
2019-04-05 18:03:04 +0100

    Implements sweeping parameter values for temperature
    Also implements performing multiple realizations for each temperature value.
    
    Finally, support is given also for multiple plots, with a warning in case of too many plots about to be created.

--- SocialInteraction.py
+++ SocialInteraction.py
@@ -9,29 +9,48 @@ import matplotlib.pyplot as plt
 
 def main():
     # Control variables
-    temperature = 0.24
+    temperatures = np.linspace(0.2, 0.3, 41, endpoint=True)  # List of temperatures to simulate
     mu = 0.5
     n_agents = 1000
     final_time = 10000
+    n_realizations = 10  # Realizations per temperature value
     initial_frequency = 0.5
     random_numbers_seed = 1
-    control_plot_results = True
-    control_write_results = False
+    control_write_results = True
+    control_plot_results = False
 
-    # Set seed for random number generator
-    rand.seed(random_numbers_seed)
+    # Confirm large number of plots with user
+    check_control_variables(control_write_results, control_plot_results, len(temperatures) * n_realizations)
 
-    # Run model
-    ts_n_agents_up = social_interaction_model(temperature, mu, n_agents, final_time, initial_frequency)
+    # Iterate over temperatures and realizations
+    i = 0
+    for temperature in temperatures:
+        ts_collector = []
+        for realization in range(n_realizations):
 
-    # Print results to file
-    if control_write_results:
-        write_results(temperature, ts_n_agents_up, "nAgentsUp")
+            # Set seed for random number generator for this realization and temperature
+            rand.seed(random_numbers_seed * i)
 
-    # Plot results
+            # Run model
+            ts_n_agents_up = social_interaction_model(temperature, mu, n_agents, final_time, initial_frequency)
+
+            # If printing results to file, collect time series for a given temperature
+            if control_write_results:
+                ts_collector.append(ts_n_agents_up)
+
+            # Plot results
+            if control_plot_results:
+                plot_results(final_time, n_agents, ts_n_agents_up, temperature)
+
+            i += 1
+
+        # Print results to file
+        if control_write_results:
+            write_results(temperature, ts_collector, "nAgentsUp")
+
+    # So that plots are shown
     if control_plot_results:
-        print("hello")
-        plot_results(final_time, n_agents, ts_n_agents_up, "nAgentsUp")
+        plt.show()
 
 
 def social_interaction_model(temperature, mu, n_agents, final_time, initial_frequency):
@@ -68,24 +87,43 @@ def social_interaction_model(temperature, mu, n_agents, final_time, initial_freq
     return ts_n_agents_up
 
 
-def write_results(temperature, time_series, file_name):
+def write_results(temperature, time_series_collector, file_name):
     """Prints results to file"""
-    with open("./Results/" + file_name + "-T" + str(temperature) + ".csv", 'w') as f:
-        for n in time_series[:-1]:
-            f.write(str(n) + "\n")
-        f.write(str(time_series[-1]))
+    with open("./Results/" + file_name + "-T{:.4f}.csv".format(temperature), 'w') as f:
+        for i, line in enumerate(zip(*time_series_collector)):
+            if i < len(time_series_collector[1]) - 1:
+                f.write("%s\n" % ", ".join([str(element) for element in line]))
+            else:
+                f.write("%s" % ", ".join([str(element) for element in line]))
 
 
-def plot_results(final_time, n_agents, time_series, label):
+def plot_results(final_time, n_agents, time_series, temperature):
     """Performs basic plotting"""
     plt.figure(figsize=(8, 6), facecolor='white')
-    plt.plot(range(final_time + 1), time_series, "o-", label=label)
+    plt.plot(range(final_time + 1), time_series, "o-")
     plt.xlim(0.0, final_time)
     plt.ylim(0.0, n_agents)
     plt.ylabel("nAgentsUp")
     plt.xlabel("Time")
+    plt.title("T = " + str(temperature))
     plt.tight_layout()
-    plt.show()
+    plt.draw()
+
+
+def check_control_variables(control_write_results, control_plot_results, number_of_plots):
+    """Checks that control parameter values make sense"""
+    if not control_write_results and not control_plot_results:
+        print("Neither writing results nor plotting them!\n"
+              "Aborting simulation.")
+        exit()
+    if control_plot_results and number_of_plots > 10:
+        reply = raw_input("Are you sure you want to generate {} plots?\n"
+                          "To confirm, type \"Y\": ".format(number_of_plots))
+        if reply != "Y":
+            print("Aborting simulation.")
+            exit()
+        else:
+            print("Continuing simulation.")
 
 
 if __name__ == "__main__":
