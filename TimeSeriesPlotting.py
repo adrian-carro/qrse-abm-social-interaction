eb37f6ff75361d278f89401aa144b0e84d2644a9
Adrián Carro <adrian.carro@gmail.com>
2019-04-05 18:24:11 +0100

    Adapts external plotting tool for multiple files and realizations

--- TimeSeriesPlotting.py
+++ TimeSeriesPlotting.py
@@ -5,20 +5,27 @@ from __future__ import division
 import matplotlib.pyplot as plt
 
 
+# Control variables
+temperature = 0.2
+
 # Reading model results from file
-nAgentsUp = []
-with open("./Results/nAgentsUp-T0.1.csv", 'r') as f:
+with open("./Results/nAgentsUp-T{:.4f}.csv".format(temperature), 'r') as f:
+    line = f.next()
+    nAgentsUp = [[] for i in range(len(line.split(",")))]
+    for column, element in zip(nAgentsUp, line.split(",")):
+        column.append(float(element))
     for line in f:
-        nAgentsUp.append(float(line))
+        for column, element in zip(nAgentsUp, line.split(",")):
+            column.append(float(element))
 
 # Plot
 fig = plt.figure(figsize=(8, 6), facecolor='white')
-plt.plot(range(len(nAgentsUp)), nAgentsUp, "o-", label="nAgentsUp")
-# plt.xlim(0.0, len(nAgentsUp))
-plt.xlim(0.0, 100)
+plt.plot(range(len(nAgentsUp[0])), nAgentsUp[0], "o-", label="nAgentsUp")
+plt.xlim(0.0, len(nAgentsUp[0]))
 plt.ylim(0.0, 1000)
 plt.ylabel("nAgentsUp")
 plt.xlabel("Time")
+plt.title("T = " + str(temperature))
 # plt.legend()
 plt.tight_layout()
 # plt.savefig('./EmploymentIncomeDist.eps', format='eps', dpi=300, bbox_inches='tight')
