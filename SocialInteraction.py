a983950756f92a4325f7ce23028d4dacda524d1e
Adrián Carro <adrian.carro@gmail.com>
2019-05-12 13:15:53 +0100

    Bring crossing times and time series back to same simulation method

--- SocialInteraction.py
+++ SocialInteraction.py
@@ -26,117 +26,54 @@ def main():
     check_control_variables(control_write_results, control_plot_results, control_compute_passage_times,
                             len(temperatures) * n_realizations)
 
-    # # # # # # # # # # Time series # # # # # # # # # #
-    if not control_compute_passage_times:
-        # Iterate over temperatures and realizations
-        i = 0
-        for temperature in temperatures:
-            ts_collector = []
-            for realization in range(n_realizations):
+    # Iterate over temperatures and realizations
+    i = 0
+    for temperature in temperatures:
+        ts_collector = []
+        crossing_times_collector = []
+        for realization in range(n_realizations):
 
-                # Set seed for random number generator for this realization and temperature
-                rand.seed(random_numbers_seed * i)
+            # Set seed for random number generator for this realization and temperature
+            rand.seed(random_numbers_seed * i)
 
-                # Run model
-                ts_n_agents_up = social_interaction_model(temperature, mu, n_agents, final_time, initial_frequency)
+            # Find approximate analytical solution for the fixed points
+            initial_guess = 0.0001  # Starting point for approximation (should be close enough to actual value)
+            x1 = optimize.newton(func, initial_guess, fprime=func_prima, args=(temperature,), tol=1.5e-09, maxiter=50)
+            x2 = 1 - x1
 
-                # If printing results to file, collect time series for a given temperature
-                if control_write_results:
-                    ts_collector.append(ts_n_agents_up)
+            # Run model
+            ts_n_agents_up, crossing_times = social_interaction_model(temperature, mu, n_agents, final_time,
+                                                                      initial_frequency, x1 * n_agents, x2 * n_agents)
 
-                # Plot results
-                if control_plot_results:
-                    plot_time_series(final_time, n_agents, ts_n_agents_up, temperature)
-
-                i += 1
-
-            # Print results to file
+            # If printing results to file, collect time series for a given temperature
             if control_write_results:
-                write_time_series(temperature, ts_collector, "nAgentsUp")
-
-        # So that plots are shown
-        if control_plot_results:
-            plt.show()
-
-    # # # # # # # # # # Passage times # # # # # # # # # #
-    else:
-        # Iterate over temperatures and realizations
-        i = 0
-        for temperature in temperatures:
-            crossing_times_collector = []
-            for realization in range(n_realizations):
-
-                # Set seed for random number generator for this realization and temperature
-                rand.seed(random_numbers_seed * i)
-
-                # Find approximate analytical solution for the fixed points
-                initial_guess = 0.0001  # Starting point for approximation (should be close enough to actual value)
-                x1 = optimize.newton(func, initial_guess, fprime=func_prima, args=(temperature,), tol=1.5e-09,
-                                     maxiter=50)
-                x2 = 1 - x1
-
-                # Run model
-                ts_n_agents_up, crossing_times = social_interaction_model_with_crossing_times(temperature, mu, n_agents,
-                                                                                              final_time,
-                                                                                              initial_frequency,
-                                                                                              x1 * n_agents,
-                                                                                              x2 * n_agents)
-
-                # If printing results to file, collect time series for a given temperature
-                if control_write_results:
+                ts_collector.append(ts_n_agents_up)
+                if control_compute_passage_times:
                     crossing_times_collector.append(crossing_times)
 
-                # Plot results
-                if control_plot_results:
+            # Plot results
+            if control_plot_results:
+                if not control_compute_passage_times:
+                    plot_time_series(final_time, n_agents, ts_n_agents_up, temperature)
+                else:
                     plot_time_series_with_crossing_times(final_time, n_agents, ts_n_agents_up, temperature, x1, x2,
                                                          crossing_times)
-                # Advance i for getting new random numbers in the next realisation
-                i += 1
 
-            # Print results to file
-            if control_write_results:
+            # Advance i for getting new random numbers in the next realisation
+            i += 1
+
+        # Print results to file
+        if control_write_results:
+            write_time_series(temperature, ts_collector, "nAgentsUp")
+            if control_compute_passage_times:
                 write_crossing_times(temperature, crossing_times_collector, "CrossingTimes")
 
-        # So that plots are shown
-        if control_plot_results:
-            plt.show()
+    # So that plots are shown
+    if control_plot_results:
+        plt.show()
 
 
-def social_interaction_model(temperature, mu, n_agents, final_time, initial_frequency):
-    # Compute initial state of the system
-    state = []
-    n_agents_up = 0
-    ts_n_agents_up = []
-    for i in range(n_agents):
-        if rand.random() <= initial_frequency:
-            state.append(1)
-            n_agents_up += 1
-        else:
-            state.append(0)
-    # Store initial state in time series
-    ts_n_agents_up.append(n_agents_up)
-
-    # Start simulation
-    t = 1
-    while t <= final_time:
-        # Update the frequency of buying for a given agent (for now, all agents have the same frequency)
-        frequency = 1/(1 + np.exp((mu - n_agents_up / n_agents) / temperature))
-        n_agents_up = 0
-        for i, s in enumerate(state):
-            # Synchronous update: all agents update their state at the same time, thus not being aware of the changes
-            # of the other agents till next time step. TODO: Confirm this point with Jangho.
-            if rand.random() <= frequency:
-                state[i] = 1
-                n_agents_up += 1
-            else:
-                state[i] = 0
-        # Store current state in time series
-        ts_n_agents_up.append(n_agents_up)
-        t += 1
-    return ts_n_agents_up
-
-
-def social_interaction_model_with_crossing_times(temperature, mu, n_agents, final_time, initial_frequency, x1, x2):
+def social_interaction_model(temperature, mu, n_agents, final_time, initial_frequency, x1, x2):
     # Compute initial state of the system
     state = []
     n_agents_up = 0
@@ -148,6 +85,7 @@ def social_interaction_model_with_crossing_times(temperature, mu, n_agents, fina
             n_agents_up += 1
         else:
             state.append(0)
+    # Store initial state in time series
     ts_n_agents_up.append(n_agents_up)
     # Store initial state as old state and set most recent equilibrium to None
     old_n_agents_up = n_agents_up
@@ -167,6 +105,8 @@ def social_interaction_model_with_crossing_times(temperature, mu, n_agents, fina
                 n_agents_up += 1
             else:
                 state[i] = 0
+        # Store current state in time series
+        ts_n_agents_up.append(n_agents_up)
         # Check if any equilibrium line has been crossed and possibly store the time
         if old_n_agents_up > x1 > n_agents_up and most_recent_equilibrium != 1:
             crossing_times.append(t)
@@ -176,7 +116,7 @@ def social_interaction_model_with_crossing_times(temperature, mu, n_agents, fina
             most_recent_equilibrium = 2
         # Update old state with current state
         old_n_agents_up = n_agents_up
-        ts_n_agents_up.append(n_agents_up)
+        # Update time
         t += 1
     return ts_n_agents_up, crossing_times
 
@@ -193,7 +133,7 @@ def write_time_series(temperature, time_series_collector, file_name):
     """Prints results to file"""
     with open("./Results/" + file_name + "-T{:.4f}.csv".format(temperature), 'w') as f:
         for i, line in enumerate(zip(*time_series_collector)):
-            if i < len(time_series_collector[1]) - 1:
+            if i < len(time_series_collector[0]) - 1:
                 f.write("%s\n" % ", ".join([str(element) for element in line]))
             else:
                 f.write("%s" % ", ".join([str(element) for element in line]))
